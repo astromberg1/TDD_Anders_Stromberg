80eadc67512a542c874471dec09adb0f86a757a5
astromberg1 <astromberg1@hotmail.com>
2017-05-22 19:31:22 +0200

    Refactoring Explained

--- TDD_Anders_Stromberg/ValidationEngine/Validator.cs
+++ TDD_Anders_Stromberg/ValidationEngine/Validator.cs
@@ -10,11 +10,28 @@ namespace ValidationEngine
 {
 public class Validator
 {
-    
+    //Refactoring is cleaning up a piece of code(e.g.improving the style, design, or algorithms), without changing(externally visible) behavior.You write tests not to make sure that the code before and after refactoring is the same, instead you write tests as an indicator that your application before and after refactoring behaves the same: The new code is compatible, and no new bugs were introduced.
+    //    Your primary concern should be to write unit tests for the public interface of your software.This interface should not change, so the tests(that are an automated check for this interface) shouldn't change either
 
-      
+    //If the purpose of your refactoring is to clean up existing code without changing existing behavior, then writing tests before refactoring is how you ensure that you have not changed the behavior of the code, if you succeed, then the tests will succeed both before and after you refactor.
+    //    The tests will help you ensure that your new work actually works.
+    //    The tests will probably also uncover cases where the original work does not work.
+    //    But how do you really do any significant refactoring without affecting behavior to some degree?
 
-   public bool ValidateEmailAddress(string emailadress)
+    //Here is a short list of a few things that might happen during refactoring:
+    //rename variable
+    //rename function
+    //add function
+    //delete function
+    //split function into two or more functions
+    //    combine two or more functions into one function
+    //    split class
+    //    combine classes
+    //    rename class
+
+
+
+        public bool ValidateEmailAddress(string emailadress)
     {
         if (string.IsNullOrEmpty(emailadress))
             return false;
